11ecdd2915c031c32312314a39de36127a5fbfcf
Rom-haguko <romagureev170@gmail.com>
2024-10-11 14:58:53 +0300

    Добавьте файлы проекта.

---
+++ zad_1_radian/zad_1_radian.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>; // для числа пи (спросить) M_PI
+
+using namespace std;
+void main()
+{
+	const double Pi = 3.141592653589793; // Переменная пи
+
+	setlocale(LC_ALL, "Rus");    //Добавляем русские символы в консоль
+	double degress, min, sec;
+	cout << "Введите градусы, минуты, секунды:\n";
+	cin >> degress >> min >> sec; // Ввод данных
+	double radian;
+	radian = (degress + (min + sec / 60) / 60 * fabs(degress) / degress) * Pi / 180;// Перевод полученных данных в радианы спросить почему не работает M_PI
+	cout << radian << endl;
+}
